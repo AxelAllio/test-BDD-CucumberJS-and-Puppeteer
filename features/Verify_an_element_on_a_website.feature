61caf9b30e3f08cf38ef8d7b6ef91c3a29b0d45f
AxelAllio <axel.allio@algoan.com>
2021-05-19 11:02:55 +0200

    AfterAll / BeforeAll

--- features/Verify_an_element_on_a_website.feature
+++ features/Verify_an_element_on_a_website.feature
@@ -2,4 +2,4 @@ Feature: Verify an element on a website
   Scenario: Verify if the title on a website correspond to what we expect
     Given The browser is open
     When I open "https://www.algoan.com/" website
-    Then the title should be "Credit decisioning as a service"
+    Then the title should be "Algoan"
--- features/stepdefs.js
+++ features/stepdefs.js
@@ -1,9 +1,14 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const assert = require('assert');
 const puppeteer = require('puppeteer')
+var {AfterAll, BeforeAll} = require('@cucumber/cucumber');
+
+BeforeAll(async function () {
+    browser = await puppeteer.launch({headless : false})
+  });
 
 Given('The browser is open', async function () {
-    this.browser = await puppeteer.launch({})
+    this.browser = browser//await puppeteer.launch({})
     this.page = await this.browser.newPage()
 });
 
@@ -18,3 +23,7 @@ Then('the title should be {string}', async function (string) {
     var res = title.split(" | ")
     assert.strictEqual(string, res[1])
 });
+
+AfterAll(async function () {
+    await browser.close();
+  });
